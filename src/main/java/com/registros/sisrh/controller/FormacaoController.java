922188fa45b3b0913af2383b7b544af97ca4e949
Raurimar <digisofthumaita@gmail.com>
2024-12-30 11:16:24 -0500

    Criação do front e configuração do tsconfig.json (strinc: false) era true

--- src/main/java/com/registros/sisrh/controller/FormacaoController.java
+++ src/main/java/com/registros/sisrh/controller/FormacaoController.java
@@ -2,6 +2,12 @@ package com.registros.sisrh.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.registros.sisrh.model.Formacao;
 import com.registros.sisrh.service.FormacaoService;
 
 @Controller
@@ -14,6 +20,11 @@ public class FormacaoController {
         Formacao formacao = new Formacao();
         model.addAttribute("novoformacao", formacao);
         return "/cadastroFormacao";
+    }
+
+    @PostMapping("/salvarFormacao")
+    public String salvarFormacao(@ModelAttribute("novoformacao") Formacao formacao) { formacaoService.criaFormacao(formacao);
+        return "redirect:/";
     }
     
 }
